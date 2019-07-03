79376795b7584bc129492d899aca5009e8a6956a
Mohammad Wasi <mdwasi15@gmail.com>
2019-07-03 22:30:32 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/',(req,res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server running at port ${PORT}`));
